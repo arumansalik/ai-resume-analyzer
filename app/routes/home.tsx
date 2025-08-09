154e8f733a8373fcee24a13b4e83ada85ac027c9
unknown <arumansalik45@gmail.com>
2025-08-10 00:24:14 +0530

    ResumeCard Updated

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -18,15 +18,17 @@ export default function Home() {
         <h1>Track Your Application & Resume Ratings</h1>
         <h2>Review your submission and Check AI-Powered feedback.</h2>
       </div>
+
+      {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+                <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+      )}
     </section>
 
-    {resumes.length > 0 && (
-        <div className="resume-section">
-          {resumes.map((resume) => (
-              <ResumeCard key={resume.id} resume={resume} />
-          ))}
-        </div>
-    )}
+
 
 
   </main>;
