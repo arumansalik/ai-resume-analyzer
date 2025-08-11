4c121e1c8c739dc071462bbcf0318b553f033c03
unknown <arumansalik45@gmail.com>
2025-08-11 22:34:16 +0530

    Uploaded File

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -21,7 +21,11 @@ const Upload = () => {
 
         setStatusText("Converting to image.....");
         const imageFile = await convertPdfToImage(file);
-        if(!imageFile.file) return setStatusText("Failed to upload file");
+        if(!imageFile.file) return setStatusText("Error: Failed to convert PDF to image");
+
+        setStatusText('Uploading the image');
+        const uploadedImage = await fs.upload([imageFile.file]);
+        if(!uploadedImage) return setStatusText("Error: Failed to upload file");
 
     }
 
